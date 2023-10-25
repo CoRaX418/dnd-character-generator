dbf36667528670d0cfb821ab03bad8ae084957b5
CoRaX <corvus-corax418@proton.me>
2023-10-25 23:19:48 +0300

    react-select and layout

--- src/app/pages/HomePage.tsx
+++ src/app/pages/HomePage.tsx
@@ -1,7 +1,8 @@
 "use client";
-import { useForm } from "react-hook-form";
+import { useController, useForm } from "react-hook-form";
 import { useGenerate } from "../hooks/useGenerate";
-import { STATS } from "../consts/consts";
+import { Alignment, STATS } from "../consts/consts";
+import ReactSelect, { Options } from "react-select";
 
 const getStatModifier = (value: number) => {
   if (value) {
@@ -10,9 +11,18 @@ const getStatModifier = (value: number) => {
   }
 }
 
+const getSelectOptions = <T extends object>(collection: T) => (
+  Object.entries(collection).map(([key, value]) => ({
+    value: key,
+    label: value
+  }))
+)
+
 export const HomePage = () => {
-  const { handleSubmit, register, watch, reset } = useForm();
+  const { handleSubmit, register, watch, reset, control } = useForm();
   const generate = useGenerate();
+
+  const { field: alignment } = useController({ name: "alignment", control })
 
   const onSubmit = () => { };
   const onGenerate = () => {
@@ -23,7 +33,7 @@ export const HomePage = () => {
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <h1>DND Character Generator</h1>
-      <div>
+      <div className="flex">
         <label>
           Имя
           <input {...register("name")} />
@@ -38,62 +48,64 @@ export const HomePage = () => {
         </label>
         <label>
           Мировоззрение
-          <input {...register("alignment")} />
+          <ReactSelect className="w-56 " options={getSelectOptions(Alignment)} value={alignment.value} onChange={alignment.onChange} />
         </label>
         <label>
           Предыстория
           <input {...register("background")} />
         </label>
       </div>
-      <div>
-        <label>
-          Сила
-          <input {...register(STATS.Strength)} type="number" />
-          {getStatModifier(watch(STATS.Strength))}
-        </label>
-        <label>
-          Ловкость
-          <input {...register(STATS.Agility)} type="number" />
-          {getStatModifier(watch(STATS.Agility))}
-        </label>
-        <label>
-          Выносливость
-          <input {...register(STATS.Constitution)} type="number" />
-          {getStatModifier(watch(STATS.Constitution))}
-        </label>
-        <label>
-          Интеллект
-          <input {...register(STATS.Intelligence)} type="number" />
-          {getStatModifier(watch(STATS.Intelligence))}
-        </label>
-        <label>
-          Мудрость
-          <input {...register(STATS.Wizdom)} type="number" />
-          {getStatModifier(watch(STATS.Wizdom))}
-        </label>
-        <label>
-          Харизма
-          <input {...register(STATS.Charisma)} type="number" />
-          {getStatModifier(watch(STATS.Charisma))}
-        </label>
-      </div>
-      <div>
-        <label>
-          Черта характера
-          <textarea {...register("trate")} />
-        </label>
-        <label>
-          Идеал
-          <textarea {...register("ideal")} />
-        </label>
-        <label>
-          Привязанность
-          <textarea {...register("attachment")} />
-        </label>
-        <label>
-          Слабость
-          <textarea {...register("weakness  ")} />
-        </label>
+      <div className="flex justify-between">
+        <div>
+          <label>
+            Сила
+            <input {...register(STATS.Strength)} type="number" />
+            {getStatModifier(watch(STATS.Strength))}
+          </label>
+          <label>
+            Ловкость
+            <input {...register(STATS.Agility)} type="number" />
+            {getStatModifier(watch(STATS.Agility))}
+          </label>
+          <label>
+            Выносливость
+            <input {...register(STATS.Constitution)} type="number" />
+            {getStatModifier(watch(STATS.Constitution))}
+          </label>
+          <label>
+            Интеллект
+            <input {...register(STATS.Intelligence)} type="number" />
+            {getStatModifier(watch(STATS.Intelligence))}
+          </label>
+          <label>
+            Мудрость
+            <input {...register(STATS.Wizdom)} type="number" />
+            {getStatModifier(watch(STATS.Wizdom))}
+          </label>
+          <label>
+            Харизма
+            <input {...register(STATS.Charisma)} type="number" />
+            {getStatModifier(watch(STATS.Charisma))}
+          </label>
+        </div>
+        <div>
+          <label>
+            Черта характера
+            <textarea {...register("trate")} />
+          </label>
+          <label>
+            Идеал
+            <textarea {...register("ideal")} />
+          </label>
+          <label>
+            Привязанность
+            <textarea {...register("attachment")} />
+          </label>
+          <label>
+            Слабость
+            <textarea {...register("weakness")} />
+          </label>
+        </div>
       </div>
       <button onClick={onGenerate}>Сгенерировать</button>
     </form>
